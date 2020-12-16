0ae50f6588202e09e2248170ed80223b5dc0db39
benjamin-andoh <benoo.andoh@gmail.com>
2020-12-16 12:26:40 +0000

    laravel ui commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public  function index()
+    {
+    	/* raw queries */
+    	// DB::insert('insert into users (name, email, password) values(?,?,?)', ['alhaji', 'alhaji@gmail.com', 'passWord']);
+    	
+    	// DB::update('update users set name = ? where id = 2', ['bitfumes']);
+
+    	// DB::delete('delete from users where id=1');
+
+    	// $users = DB::select('select * from users');
+
+
+    	/* eloquentORM */
+    // 1. create 
+    	// $user = new User();
+    	// $user->name  = 'nert';
+    	// $user->email  = 'htbn@gmail.com';
+    	// $user->password  = bcrypt('assWord');
+    	// $user->save();
+
+
+	// 2. delete 
+    	// $user = User::where('id', 4)->delete();
+
+	// 3. update
+    	// $user = User::where('id', 3)->update(['name' => "fuses"]);
+
+
+    	/* alternative create*/
+    	$data = [
+    		'name' => 'Elonmust',
+    		'email' => 'elonate@gmail.com',
+    		'password' => 'aSsWorD',
+    	];
+    	// User::create($data);
+    
+    //4. select all 
+    	$user = User::all();
+
+    	return $user;
+    	
+    }
+}
